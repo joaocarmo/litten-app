ab233ccebd824f8c9436d23ff13236fe5d6a8040
Joao Carmo <17721880+joaocarmo@users.noreply.github.com>
2020-04-27 18:50:36 +0100

    :card_file_box: Added Firebase
    Started to connect the app with Firebase

--- README.md
+++ README.md
@@ -26,10 +26,17 @@ yarn test
 yarn prepare-data
 ```
 
+## Backend
+
+Currently, the project relies on a [Firebase][firebase] infrastructure.
+
 ## License
 
 The project's source code is licensed as [AGPL-3.0][1], but the images are
 [not licensed][2].
 
-[1]: ./LICENSE
-[2]: ./lib/images/README.md
+<!-- References -->
+
+[firebase]: https://firebase.google.com/
+[license]: ./LICENSE
+[licenseimgs]: ./lib/images/README.md
--- ios/litten/AppDelegate.m
+++ ios/litten/AppDelegate.m
@@ -4,6 +4,7 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 #import "RNSplashScreen.h"
+#import <Firebase.h>
 
 #if DEBUG
 #import <FlipperKit/FlipperClient.h>
@@ -31,7 +32,10 @@ static void InitializeFlipper(UIApplication *application) {
 #if DEBUG
   InitializeFlipper(application);
 #endif
-
+  if ([FIRApp defaultApp] == nil) {
+    [FIRApp configure];
+  }
+  
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"litten"
