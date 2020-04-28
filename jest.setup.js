f5586a5e8d462a28a493738a742d3d2910460482
Joao Carmo <17721880+joaocarmo@users.noreply.github.com>
2020-04-29 00:14:54 +0100

    :white_check_mark: Improved the testing framework

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,3 @@
-import mockAsyncStorage from '@react-native-community/async-storage/jest/async-storage-mock'
-
-jest.mock('@react-native-community/async-storage', () => mockAsyncStorage)
-
-jest.mock('react-native-device-info', () => ({ getModel: jest.fn() }))
+jest.mock('redux-persist/integration/react', () => ({
+  PersistGate: ({ children }) => children,
+}))
