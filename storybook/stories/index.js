e01bd4753bf2b025cb83a7d3ff4d6da9ab223fc4
Joao Carmo <17721880+joaocarmo@users.noreply.github.com>
2020-07-22 23:54:48 +0100

    :lipstick: Finished the 'Favourites' screen

--- storybook/stories/index.js
+++ storybook/stories/index.js
@@ -2,10 +2,8 @@ import React from 'react'
 import { storiesOf } from '@storybook/react-native'
 import { UIButton } from 'ui-elements'
 
-storiesOf('Button', module).add('primary button', () => (
-  <UIButton>Primary Button</UIButton>
-))
-
-storiesOf('Button', module).add('secondary button', () => (
-  <UIButton secondary>Secondary Button</UIButton>
-))
+storiesOf('Button', module)
+  .add('primary button', () => <UIButton>Primary Button</UIButton>)
+  .add('secondary button', () => (
+    <UIButton secondary>Secondary Button</UIButton>
+  ))
