d83b7954f4139c6ea57e35260daceebbb1de4997
Joao Carmo <17721880+joaocarmo@users.noreply.github.com>
2020-11-22 21:03:34 +0000

    :robot: Fixed the Select component on Android

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,7 @@ module.exports = {
     '<rootDir>/jest.setup.js',
   ],
   transformIgnorePatterns: [
-    'node_modules/(?!react-native|@react-native-community/*|react-navigation|react-navigation-stack|@react-navigation/.*)',
+    'node_modules/(?!react-native|@react-native-community/*|react-navigation|react-navigation-stack|@react-navigation/.*|@react-native-picker/.*)',
   ],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
