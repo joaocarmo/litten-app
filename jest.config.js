6632b1e881a6fae1ad766af0c75166e29d8b5943
Joao Carmo <17721880+joaocarmo@users.noreply.github.com>
2020-11-29 22:23:29 +0000

    :bento: Started to convert the PNGs to SVGs for improved visuals

--- jest.config.js
+++ jest.config.js
@@ -11,5 +11,6 @@ module.exports = {
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/lib/__mocks__/file-mock.js',
+    '\\.svg': '<rootDir>/__mocks__/svgMock.js',
   },
 }
