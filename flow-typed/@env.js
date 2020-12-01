748cf01e47371fcd6db07ed9b908efadc9169519
Joao Carmo <17721880+joaocarmo@users.noreply.github.com>
2020-12-01 21:49:36 +0000

    :sparkles: Feedback is posted to Slack during development

--- flow-typed/@env.js
+++ flow-typed/@env.js
@@ -1,8 +1,9 @@
 declare module '@env' {
-  declare export var WEB_APP_BASE: string;
   declare export var FIREBASE_AUTH_EMULATOR_HOST: string;
   declare export var FIRESTORE_EMULATOR_HOST: string;
   declare export var FIRESTORE_EMULATOR_PERSISTENCE: string;
-  declare export var USE_FIREBASE_EMULATOR: string;
   declare export var GOOGLE_API_KEY: string;
+  declare export var SLACK_WEBHOOK_URL: string;
+  declare export var USE_FIREBASE_EMULATOR: string;
+  declare export var WEB_APP_BASE: string;
 }
