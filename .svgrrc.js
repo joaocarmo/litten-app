6ec84fe8f6e4e84e656501aa9f844ad46dbd9033
Joao Carmo <17721880+joaocarmo@users.noreply.github.com>
2020-12-01 17:18:46 +0000

    :bento: Continued to update the PNGs to SVGs

--- .svgrrc.js
+++ .svgrrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   replaceAttrValues: {
     '#000': '{props.fill}',
+    '#000': '{props.stroke}',
   },
 }
--- jest.config.js
+++ jest.config.js
@@ -9,8 +9,8 @@ module.exports = {
     'node_modules/(?!react-native|@react-native-community/*|react-navigation|react-navigation-stack|@react-navigation/.*|@react-native-picker/.*)',
   ],
   moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/lib/__mocks__/file-mock.js',
-    '\\.svg': '<rootDir>/__mocks__/svgMock.js',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/lib/__mocks__/fileMock.js',
+    '\\.svg$': '<rootDir>/lib/__mocks__/svgMock.js',
   },
 }
