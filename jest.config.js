7141d64580747cc0949df4677b01004aea415902
Joao Carmo <17721880+joaocarmo@users.noreply.github.com>
2020-12-05 00:46:24 +0000

    :white_check_mark: Updated the Jest setup to support React Navigation

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,9 @@
 module.exports = {
   preset: 'react-native',
   timers: 'fake',
-  setupFiles: [
-    '<rootDir>/node_modules/react-native-gesture-handler/jestSetup.js',
-    '<rootDir>/jest.setup.js',
-  ],
+  setupFiles: ['<rootDir>/jest.setup.js'],
   transformIgnorePatterns: [
-    'node_modules/(?!react-native|@react-native-community/*|react-navigation|react-navigation-stack|@react-navigation/.*|@react-native-picker/.*)',
+    'node_modules/(?!react-native|@react-native-community/*|@react-native-picker/.*)',
   ],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
