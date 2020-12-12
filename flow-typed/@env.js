152aaa3673e52e36bd2d0ddc27f310e7b6545cd7
Joao Carmo <17721880+joaocarmo@users.noreply.github.com>
2020-12-12 22:50:05 +0000

    :sparkles: Finished the report feature
    Creates service tickets in Jira (production)

--- flow-typed/@env.js
+++ flow-typed/@env.js
@@ -3,6 +3,9 @@ declare module '@env' {
   declare export var FIRESTORE_EMULATOR_HOST: string;
   declare export var FIRESTORE_EMULATOR_PERSISTENCE: string;
   declare export var GOOGLE_API_KEY: string;
+  declare export var JIRA_API_KEY: string;
+  declare export var JIRA_EMAIL: string;
+  declare export var JIRA_TICKET_URL: string;
   declare export var SLACK_WEBHOOK_URL: string;
   declare export var USE_FIREBASE_EMULATOR: string;
   declare export var USE_REDUX_DEVTOOLS_LOCAL_SERVER: string;
