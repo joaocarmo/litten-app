037d46942e370f237e14fe6e561a095590c33081
João Carmo <17721880+joaocarmo@users.noreply.github.com>
2021-02-25 22:44:20 +0000

    :green_heart: Added the missing environment variables

--- flow-typed/@env.js
+++ flow-typed/@env.js
@@ -5,13 +5,9 @@ declare module '@env' {
   declare export var GOOGLE_API_KEY: string;
   declare export var IS_BETA_RELASE: String;
   declare export var JIRA_API_KEY: string;
-  declare export var JIRA_APPEND_ATTACHMENT: string;
   declare export var JIRA_EMAIL: string;
-  declare export var JIRA_TICKET_URL: string;
-  declare export var JIRA_UPLOAD_ATTACHMENT: string;
   declare export var SLACK_WEBHOOK_URL: string;
   declare export var USE_FIREBASE_EMULATOR: string;
   declare export var USE_GRAVATAR: string;
   declare export var USE_REDUX_DEVTOOLS_LOCAL_SERVER: string;
-  declare export var WEB_APP_BASE: string;
 }
