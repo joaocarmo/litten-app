709fe0708ab48e54501ac35b1c167b6fa747cd73
João Carmo <17721880+joaocarmo@users.noreply.github.com>
2021-03-15 23:27:15 +0000

    Upgrade to react native 0.64.0 (#3)
    * :green_heart: Added a GH PAT instead of the default GH token
    
    * :construction_worker: Tests will run for both master and next
    
    * :arrow_up: Updated some dependencies
    
    * :arrow_up: Updated the required files
    
    * :white_check_mark: Fixed the tests

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 const globalConfig = {
   transformIgnorePatterns: [
-    'node_modules/(?!react-native|@react-native-community/*|@react-native-picker/.*)',
+    'node_modules/(?!(@react-native|react-native)|@react-native-community/*|@react-native-picker/.*)',
   ],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
