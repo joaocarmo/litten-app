2a630a46228d134859bb537e82218640775f0c52
João Carmo <kingjoao@gmail.com>
2021-12-26 12:08:34 +0000

    :wrench: Added babel cache

--- babel.config.js
+++ babel.config.js
@@ -1,35 +1,39 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'dotenv-import',
-      {
-        allowUndefined: false,
-        moduleName: '@env',
-        path: '.env',
-        safe: false,
-        whitelist: [
-          'FIREBASE_AUTH_EMULATOR_HOST',
-          'FIRESTORE_EMULATOR_HOST',
-          'FIRESTORE_EMULATOR_PERSISTENCE',
-          'GOOGLE_API_KEY',
-          'IS_BETA_RELASE',
-          'JIRA_API_KEY',
-          'JIRA_EMAIL',
-          'SLACK_WEBHOOK_URL',
-          'USE_FIREBASE_EMULATOR',
-          'USE_GRAVATAR',
-          'USE_REDUX_DEVTOOLS_LOCAL_SERVER',
-        ],
-      },
+module.exports = (api) => {
+  api.cache(true)
+
+  return {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+      [
+        'dotenv-import',
+        {
+          allowUndefined: false,
+          moduleName: '@env',
+          path: '.env',
+          safe: false,
+          whitelist: [
+            'FIREBASE_AUTH_EMULATOR_HOST',
+            'FIRESTORE_EMULATOR_HOST',
+            'FIRESTORE_EMULATOR_PERSISTENCE',
+            'GOOGLE_API_KEY',
+            'IS_BETA_RELASE',
+            'JIRA_API_KEY',
+            'JIRA_EMAIL',
+            'SLACK_WEBHOOK_URL',
+            'USE_FIREBASE_EMULATOR',
+            'USE_GRAVATAR',
+            'USE_REDUX_DEVTOOLS_LOCAL_SERVER',
+          ],
+        },
+      ],
+      [
+        '@babel/plugin-transform-react-jsx',
+        {
+          runtime: 'automatic',
+        },
+      ],
+      ['macros'],
+      'react-native-reanimated/plugin',
     ],
-    [
-      '@babel/plugin-transform-react-jsx',
-      {
-        runtime: 'automatic',
-      },
-    ],
-    ['macros'],
-    'react-native-reanimated/plugin',
-  ],
+  }
 }
