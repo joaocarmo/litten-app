cb5c2f25ceb8f60a24e37333e501f4bb80823fb9
João Carmo <kingjoao@gmail.com>
2022-02-14 22:50:16 +0000

    :arrow_up: Updated a dependency's namespace

--- lib/__mocks__/@react-native-community/async-storage.ts
+++ lib/__mocks__/@react-native-community/async-storage.ts
@@ -1,3 +1,3 @@
-import asyncStorageMock from '@react-native-community/async-storage/jest/async-storage-mock'
+import asyncStorageMock from '@react-native-async-storage/async-storage/jest/async-storage-mock'
 
 export default asyncStorageMock
--- lib/config/store/index.ts
+++ lib/config/store/index.ts
@@ -1,5 +1,5 @@
 import { APP_IS_DEV, USE_REDUX_DEVTOOLS_LOCAL_SERVER } from '@utils/env'
-import AsyncStorage from '@react-native-community/async-storage'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import appConfig from '../../../app.json'
 
 const devToolsConfig: any = USE_REDUX_DEVTOOLS_LOCAL_SERVER
