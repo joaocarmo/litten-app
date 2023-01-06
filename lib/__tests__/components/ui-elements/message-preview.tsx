fa0ab8f9882a370a251fa0a03c971830c848684a
João Carmo <17721880+joaocarmo@users.noreply.github.com>
2023-01-06 13:36:42 +0000

    :green_heart: Upgrades CI workflows (#149)

--- lib/__tests__/components/ui-elements/message-preview.tsx
+++ lib/__tests__/components/ui-elements/message-preview.tsx
@@ -12,7 +12,7 @@ publishing packages and web page editors now use Lorem Ipsum as
 their default model text, and a search for 'lorem ipsum' will
 uncover many web sites still in their infancy. Various versions
 have evolved over the years, sometimes by accident, sometimes on
-purpose (injected humour and the like). \
+purpose (injected humour and the like).\
 `
 
 describe('Snapshot test for the "UIMessage" component', () => {
