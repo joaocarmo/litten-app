b83cf90be329b98587e2256e1b6c410d47b9dcdc
João Carmo <kingjoao@gmail.com>
2023-05-14 23:42:03 +0100

    :white_check_mark: Fix failing tests

--- lib/__tests__/components/ui-elements/option.tsx
+++ lib/__tests__/components/ui-elements/option.tsx
@@ -1,9 +1,14 @@
 import { render } from '@utils/tests/react-native'
+import UISelectPlatform from '@ui-elements/inner-components/select'
+import UISelectPlatformAndroid from '@ui-elements/inner-components/select/select.android'
+import UISelectPlatformIOS from '@ui-elements/inner-components/select/select.ios'
 import { UIOption } from '@ui-elements'
 
-jest.mock('react-native/Libraries/Utilities/Platform', () => ({
-  OS: 'ios',
-}))
+jest.mock('@ui-elements/inner-components/select')
+
+const mockUISelectPlatform = UISelectPlatform as jest.MockedFunction<
+  typeof UISelectPlatform
+>
 
 describe('Snapshot test for the "UISwitch" component', () => {
   const items = [
@@ -20,28 +25,41 @@ describe('Snapshot test for the "UISwitch" component', () => {
   ]
   const selectedValue = 'one'
 
-  it('renders correctly without description', () => {
-    const element = render(
-      <UIOption
-        items={items}
-        label="Some text here"
-        selectedValue={selectedValue}
-      />,
-    )
+  const platform = [
+    ['ios', UISelectPlatformIOS],
+    ['android', UISelectPlatformAndroid],
+  ]
 
-    expect(element.toJSON()).toMatchSnapshot()
-  })
+  describe.each(platform)('platform %s', (_, Component) => {
+    beforeEach(() => {
+      mockUISelectPlatform.mockImplementation((props) => (
+        <Component {...props} />
+      ))
+    })
 
-  it('renders correctly with description', () => {
-    const element = render(
-      <UIOption
-        description="Some more text here"
-        items={items}
-        label="Some text here"
-        selectedValue={selectedValue}
-      />,
-    )
+    it('renders correctly without description', () => {
+      const element = render(
+        <UIOption
+          items={items}
+          label="Some text here"
+          selectedValue={selectedValue}
+        />,
+      )
 
-    expect(element.toJSON()).toMatchSnapshot()
+      expect(element.toJSON()).toMatchSnapshot()
+    })
+
+    it('renders correctly with description', () => {
+      const element = render(
+        <UIOption
+          description="Some more text here"
+          items={items}
+          label="Some text here"
+          selectedValue={selectedValue}
+        />,
+      )
+
+      expect(element.toJSON()).toMatchSnapshot()
+    })
   })
 })
